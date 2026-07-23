ac8b558be16fb618141b05827e600e8f2a5e22d3
breznaM <breznamatej22@gmail.com>
2026-07-23 09:42:34 +0200

    Add currency buttons switching functionality

--- travel_cost_calculator/travel-cost-calculator.js
+++ travel_cost_calculator/travel-cost-calculator.js
@@ -1,10 +1,10 @@
-const distanceInputElem = document.querySelector('.js-distance');
-const consumptionInputElem = document.querySelector('.js-consumption');
-const fuelPriceInptuElem = document.querySelector('.js-fuel-price');
-
 let distance = 0;
 let consumption = 0;
 let fuelPrice = 0;
+
+const distanceInputElem = document.querySelector('.js-distance');
+const consumptionInputElem = document.querySelector('.js-consumption');
+const fuelPriceInptuElem = document.querySelector('.js-fuel-price');
 
 distanceInputElem.addEventListener('change', () => {
   distance = Number(distanceInputElem.value);
@@ -19,7 +19,26 @@ fuelPriceInptuElem.addEventListener('change', () => {
   calculate();
 });
 
-//CALCULATION
+const currencyInputElemCZK = document.querySelector('.js-currency-input-czk');
+const currencyInputElemEUR = document.querySelector('.js-currency-input-eur');
+const currencyInputElemUSD = document.querySelector('.js-currency-input-usd');
+
+currencyInputElemCZK.addEventListener('click', () => {
+  currencyInputElemCZK.classList.add('is-active');
+  currencyInputElemEUR.classList.remove('is-active');
+  currencyInputElemUSD.classList.remove('is-active');
+});
+currencyInputElemEUR.addEventListener('click', () => {
+  currencyInputElemEUR.classList.add('is-active');
+  currencyInputElemCZK.classList.remove('is-active');
+  currencyInputElemUSD.classList.remove('is-active');
+});
+currencyInputElemUSD.addEventListener('click', () => {
+  currencyInputElemUSD.classList.add('is-active');
+  currencyInputElemEUR.classList.remove('is-active');
+  currencyInputElemCZK.classList.remove('is-active');
+});
+
 function calculate() {
   const fuelConsumption = ((distance / 100) * consumption);
   const travelExpenses = (fuelConsumption * fuelPrice);
