fafb9233d9ee16e1b659458f0ddb608d27326562
breznaM <breznamatej22@gmail.com>
2026-07-24 11:26:29 +0200

    Add localStorage

--- travel_cost_calculator/travel-cost-calculator.js
+++ travel_cost_calculator/travel-cost-calculator.js
@@ -1,36 +1,53 @@
 import { currencies, consumptionUnits, linkedUnits } from "./data.js";
 
-//INPUT VARIABLES
-let distance = 0;
-let consumption = 0;
-let fuelPrice = 0;
-let passengers = 1;
-
 //INPUT ELEMENTS
 const distanceInputElem = document.querySelector('.js-distance');
 const consumptionInputElem = document.querySelector('.js-consumption');
 const fuelPriceInputElem = document.querySelector('.js-fuel-price');
 const passengersInputElem = document.querySelector('.js-passengers');
 
-//CHOSEN UNITS AND CURRENCY
-let chosenDistanceUnit = 'kms';
-let chosenConsumptionUnit = 'l100km';
-let chosenCurrency = 'czk';
+//INPUT VARIABLES
+let distance = getFromLocalStorage('distance') || ''
+let consumption = getFromLocalStorage('consumption') || '';
+let fuelPrice = getFromLocalStorage('fuelPrice') || '';
+let passengers = getFromLocalStorage('passengers') || 1;
+//display from localStorage
+distanceInputElem.value = distance;
+consumptionInputElem.value = consumption;
+fuelPriceInputElem.value = fuelPrice;
+passengersInputElem.value = passengers;
 
+//CHOSEN UNITS AND CURRENCY
+let chosenDistanceUnit = getFromLocalStorage('chosenDistanceUnit') || 'kms';
+let chosenConsumptionUnit = getFromLocalStorage('chosenConsumptionUnit') || 'l100km';
+let chosenCurrency = getFromLocalStorage('chosenCurrency') || 'czk';
+//display from localStorage
+const distanceUnitElemToActivate = document.querySelector(`.js-${chosenDistanceUnit}-unit`);
+const consumptionUnitElemToActivate = document.querySelector(`.js-${chosenConsumptionUnit}-unit`);
+const currencyElemToActivate = document.querySelector(`.js-${chosenCurrency}-currency`);
+activateButton('js-distance-input', distanceUnitElemToActivate);
+activateButton('js-consumption-input', consumptionUnitElemToActivate);
+activateButton('js-currency-input', currencyElemToActivate);
+
+//USER INPUT
 distanceInputElem.addEventListener('input', () => {
   distance = Number(distanceInputElem.value);
+  setToLocalStorage('distance', distance);
   calculate();
 });
 consumptionInputElem.addEventListener('input', () => {
   consumption = Number(consumptionInputElem.value);
+  setToLocalStorage('consumption', consumption);
   calculate();
 });
 fuelPriceInputElem.addEventListener('input', () => {
   fuelPrice = Number(fuelPriceInputElem.value);
+  setToLocalStorage('fuelPrice', fuelPrice);
   calculate();
 });
 passengersInputElem.addEventListener('input', () => {
   passengers = Number(passengersInputElem.value);
+  setToLocalStorage('passengers', passengers);
   calculate();
 });
 
@@ -42,10 +59,12 @@ distanceUnitsInputElems.forEach((element) => {
     //switching buttons
     activateButton('js-distance-input', element);
     chosenDistanceUnit = element.dataset.distance;
+    setToLocalStorage('chosenDistanceUnit', chosenDistanceUnit);
 
     //update chosenDistanceUnit
     if (chosenDistanceUnit === 'kms') chosenConsumptionUnit = 'l100km';
     else if (chosenDistanceUnit === 'miles') chosenConsumptionUnit = 'mpg';
+    setToLocalStorage('chosenConsumptionUnit', chosenConsumptionUnit);
 
     switchRelatedUnits('consumption', chosenDistanceUnit);
 
@@ -59,14 +78,18 @@ distanceUnitsInputElems.forEach((element) => {
 
 //ROUND TRIP
 const roundTripInputElem = document.querySelector('.js-round-trip');
-let isChecked;
+let isChecked = getFromLocalStorage('isChecked');
+if (isChecked) roundTripInputElem.classList.add('is-active');
+
 roundTripInputElem.addEventListener('click', () => {
   if (!isChecked) {
     roundTripInputElem.classList.add('is-active');
     isChecked = true;
+    setToLocalStorage('isChecked', isChecked);
   } else if (isChecked) {
     roundTripInputElem.classList.remove('is-active');
     isChecked = false;
+    setToLocalStorage('isChecked', isChecked);
   }
   calculate();
 });
@@ -79,10 +102,12 @@ consumptionUnitsInputElems.forEach((element) => {
     //switching buttons
     activateButton('js-consumption-input', element);
     chosenConsumptionUnit = element.dataset.consumption;
+    setToLocalStorage('chosenConsumptionUnit', chosenConsumptionUnit);
 
     //update chosenDistanceUnit
     if (chosenConsumptionUnit === 'l100km') chosenDistanceUnit = 'kms';
     else if (chosenConsumptionUnit === 'mpg') chosenDistanceUnit = 'miles';
+    setToLocalStorage('chosenDistanceUnit', chosenDistanceUnit);
 
     switchRelatedUnits('distance', chosenConsumptionUnit);
 
@@ -101,6 +126,7 @@ currencyInputElems.forEach((element) => {
     //switching buttons
     activateButton('js-currency-input', element);
     chosenCurrency = element.dataset.currency;
+    setToLocalStorage('chosenCurrency', chosenCurrency);
 
     //update input placeholders when changing currency
     fuelPriceInputElem.placeholder = currencies[chosenCurrency].placeholder;
@@ -109,13 +135,24 @@ currencyInputElems.forEach((element) => {
   });
 });
 
+calculate();
+
+function setToLocalStorage(id, variable) {
+  return localStorage.setItem(`${id}`, JSON.stringify(variable));
+}
+
+function getFromLocalStorage(id) {
+  return JSON.parse(localStorage.getItem(id));
+}
+
 function switchRelatedUnits(what, from) {
   const elementToActivate = document.querySelector(linkedUnits[from]);
   activateButton(`js-${what}-input`, elementToActivate);
 }
 
 function activateButton(groupSelector, elementToActivate) {
-  document.querySelector(`.${groupSelector}.is-active`).classList.remove('is-active');
+  const currentlyActive = document.querySelector(`.${groupSelector}.is-active`);
+  if (currentlyActive) currentlyActive.classList.remove('is-active');
   elementToActivate.classList.add('is-active');
 }
 
