d5181bd86d49794e80a98cb6d07c7630e61d0008
breznaM <breznamatej22@gmail.com>
2026-08-19 19:40:24 +0200

    Fix language switch button error

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,7 +1,11 @@
 import * as Model from './model.js';
 import * as View from './view.js';
+import { translations } from './translations.js';
 
 function init() {
+  View.applyTranslations(Model.state.language);
+  View.languageBtnSwitch(Model.state.language === 'en' ? View.elements.englishButton : View.elements.czechButton);
+
   View.elements.englishButton.addEventListener('click', () => {
     const selectedLanguage = 'en';
     Model.updateLanguage(selectedLanguage);
