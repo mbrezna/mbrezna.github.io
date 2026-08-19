cd9ff52bcbf4de65e368efc8621b8ea4934c4810
breznaM <breznamatej22@gmail.com>
2026-08-19 20:55:16 +0200

    Add missing page content translations

--- scripts/translations.js
+++ scripts/translations.js
@@ -29,7 +29,7 @@ export const translations = {
     semestralMapParagraph: 'Cartographic project developed within the Digital Cartography course, resulting in a large-format (A2) desk map designed for strategic decision-making by public sector professionals.',
     travelCalculatorTitle: 'Travel cost calculator',
     travelCalculatorTime: 'July 2026',
-    travelCalculatorParagraph: 'An JavaScript web application built to calculate and share travel expenses. It supports multiple languages and enables unit conversion, saving trip history, and generating QR codes for easy cost splitting. Focused on Vanilla JavaScript, this project helped me gain an understanding of DOM manipulation, event handling, and modules. To ensure clean code organization, I implemented the MVC (Model-View-Controller) architecture.',
+    travelCalculatorParagraph: 'A JavaScript web application built to calculate and share travel expenses. It supports multiple languages and enables unit conversion, saving trip history, and generating QR codes for easy cost splitting. Focused on Vanilla JavaScript, this project helped me gain an understanding of DOM manipulation, event handling, and modules. To ensure clean code organization, I implemented the MVC (Model-View-Controller) architecture.',
 
     // buttons
     liveDemoButton: 'Live demo',
@@ -46,9 +46,10 @@ export const translations = {
     grammarSchoolTime: `<time datetime="2017">2017</time> – <time datetime="2025">2025</time>`,
     grammarSchoolParagraph: '8-Year General Grammar School',
     UPOLtitle: 'Palacký University Olomouc',
-    UPOLtime: `<time datetime="2017">2025</time> – Present`,
+    UPOLtime: `<time datetime="2025">2025</time> – Present`,
     UPOLparagraph: `Bachelor's Degree in Geoinformatics and Cartography`,
   },
+
   cs: {
     //header + section titles
     experienceSection: 'Zkušenosti',
@@ -57,49 +58,48 @@ export const translations = {
     contactLink: 'Kontakt',
 
     // hero section
-    heroTitle: `Student <br><strong class="highlight">Geoinformatiky a Kartografie</strong>`,
+    heroTitle: `Student <strong class="highlight">geoinformatiky <br>a kartografie</strong>`,
     heroSubtitle: `Se zaměřením na <br><em class="highlight">front-end webové technologie</em>`,
 
     // about section
-    aboutFirstParagraph: 'Jsem studentem geoinformatiky na Univerzitě Palackého v Olomouci v České republice. Roky jsem se s velkým zápalem věnoval běhu na lyžích, ale s nástupen na vysokou školu mě studium pohltilo naplno. Disciplínu a cílevědomost, kterou jsem si za roky vrcholového sportu vybudoval, nyní přenáším do studia geoinformatiky a webových technologií.',
+    aboutFirstParagraph: 'Jsem studentem geoinformatiky na Univerzitě Palackého v Olomouci v České republice. Roky jsem se s velkým zápalem věnoval běhu na lyžích, ale s nástupem na vysokou školu mě studium pohltilo naplno. Disciplínu a cílevědomost, kterou jsem si za roky vrcholového sportu vybudoval, nyní přenáším do studia geoinformatiky a webových technologií.',
 
-    aboutSecondParagraph: 'Během studia si buduji pevné základy v analýze prostorových dat, GIS a tvorbě map. Protože mě ale nejvíce naplňuje propojování těchto oborů s IT, věnuji svůj volný čas samostudiu front-endového vývoje (HTML, CSS a JavaScript). Mými dalšími cíli je rozšířit své dovednosti o framework React a proniknout i do back-endového vývoje pomocí jazyka Python a práce s databázemi (SQL/MySQL).',
+    aboutSecondParagraph: 'Během studia si buduji pevné základy v analýze prostorových dat, GIS a tvorbě map. Protože mě ale nejvíce naplňuje propojování těchto oborů s IT, věnuji svůj volný čas samostudiu front-endového vývoje (HTML, CSS a JavaScript). Mým dalším cílem je rozšířit své dovednosti o framework React a proniknout i do back-endového vývoje pomocí jazyka Python a práce s databázemi (SQL/MySQL).',
 
     aboutThirdParagraph: 'Aktuálně hledám příležitosti pro studentské stáže nebo zkrácené úvazky, kde bych mohl své dosavadní znalosti uvést do praxe. Těším se na možnost pomáhat s reálnými projekty a nabrat nové zkušenosti.',
 
-    personalMotto: '"Vše je možné, pokud je vaše \‚chci\‘ dostatečně silné."',
+    personalMotto: '"Vše je možné, pokud je vaše ‚chci‘ dostatečně silné."',
 
     // experience section
     crossCountryTitle: 'Běžec na lyžích na mezinárodní úrovni',
-    crossCountryParagraph: 'Balancing a demanding daily training schedule with academic studies taught me how to manage my time effectively. Years of long-term athletic preparation built my goal-oriented mindset.',
-    questionaryTitle: 'Field Data Collector',
-    questionaryTime: 'October 2025',
-    questionaryParagraph: 'Sustainable Urban Mobility Plan City of Olomouc',
+    crossCountryParagraph: 'Skloubení náročného každodenního tréninku s vysokoškolským studiem mě naučilo efektivně si organizovat čas. Roky dlouhodobé sportovní přípravy ve mně vybudovaly cílevědomý přístup.',
+    questionaryTitle: 'Pracovník pro sběr dat v terénu',
+    questionaryTime: 'říjen 2025',
+    questionaryParagraph: 'Dopravní průzkum v rámci Plánu udržitelné městské mobility Olomouc.',
 
     // projects section
-    semestralMapTitle: 'Crisis and health infrastructure in Norway and Sweden',
-    semestralMapTime: 'May 2026',
-    semestralMapParagraph: 'Cartographic project developed within the Digital Cartography course, resulting in a large-format (A2) desk map designed for strategic decision-making by public sector professionals.',
-    travelCalculatorTitle: 'Travel cost calculator',
-    travelCalculatorTime: 'July 2026',
-    travelCalculatorParagraph: 'An JavaScript web application built to calculate and share travel expenses. It supports multiple languages and enables unit conversion, saving trip history, and generating QR codes for easy cost splitting. Focused on Vanilla JavaScript, this project helped me gain an understanding of DOM manipulation, event handling, and modules. To ensure clean code organization, I implemented the MVC (Model-View-Controller) architecture.',
+    semestralMapTitle: 'Krizová a zdravotnická infrastruktura v Norsku a Švédsku',
+    semestralMapTime: 'květen 2026',
+    semestralMapParagraph: 'Kartografický projekt vytvořený v rámci předmětu Digitální kartografie. Jeho výsledkem je velkoformátová (A2) stolní mapa navržená pro strategické rozhodování pracovníků veřejného sektoru.',
+    travelCalculatorTitle: 'Kalkulačka cestovních nákladů',
+    travelCalculatorTime: 'červenec 2026',
+    travelCalculatorParagraph: 'JavaScriptová webová aplikace vytvořená k výpočtu a sdílení cestovních nákladů. Podporuje více jazyků a umožňuje převody jednotek, ukládání historie cest a generování QR kódů pro snadné rozdělení výdajů. Tento projekt, zaměřený na čistý (Vanilla) JavaScript, mi pomohl osvojit si manipulaci s DOM, práci s událostmi (event handling) a využití modulů. Pro zajištění přehledné struktury kódu jsem implementoval architekturu MVC (Model-View-Controller).',
 
     // buttons
-    liveDemoButton: 'Live demo',
-    githubRepoButton: 'Github repo',
-    personalWebTitle: 'Personal web',
-    personalWebTime: 'August 2026',
-    personalWebParagraph: 'Portfolio in HTML, CSS and JavaScript',
-    websiteButton: 'Website',
-    departmentButton: 'Department',
-    curriculumButton: 'Curriculum',
+    liveDemoButton: 'Spustit aplikaci',
+    githubRepoButton: 'Zdrojový kód',
+    personalWebTitle: 'Osobní web',
+    personalWebTime: 'srpen 2026',
+    personalWebParagraph: 'Portfolio v HTML, CSS a JavaScriptu',
+    websiteButton: 'Web',
+    departmentButton: 'Katedra',
+    curriculumButton: 'Studijní plán',
 
     // education section
-    grammarSchoolTitle: 'Grammar school Třebíč',
-    grammarSchoolTime: `<time datetime="2017">2017</time> – <time datetime="2025">2025</time>`,
-    grammarSchoolParagraph: '8-Year General Grammar School',
-    UPOLtitle: 'Palacký University Olomouc',
-    UPOLtime: `<time datetime="2017">2025</time> – Present`,
-    UPOLparagraph: `Bachelor's Degree in Geoinformatics and Cartography`,
+    grammarSchoolTitle: 'Gymnázium Třebíč',
+    grammarSchoolParagraph: 'Osmileté všeobecné gymnázium',
+    UPOLtitle: 'Univerzita Palackého v Olomouci',
+    UPOLtime: `<time datetime="2025">2025</time> – dosud`,
+    UPOLparagraph: `Bakalářský obor Geoinformatika a kartografie`,
   }
 }
