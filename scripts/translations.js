d38c3ad908533245e0f4ba51b097231a90d511dc
breznaM <breznamatej22@gmail.com>
2026-08-22 19:49:41 +0200

    Web content revision and text highlighting with <strong> tag

--- scripts/translations.js
+++ scripts/translations.js
@@ -12,32 +12,32 @@ export const translations = {
 
     // about section
     aboutFirstParagraph: 'I am a student of geoinformatics at Palacký University Olomouc, Czechia. For years, I was a competitive cross-country skier with great enthusiasm, but as soon as I started university, my studies took my full attention. The discipline and determination I built during my years in elite sports are qualities I now apply to my studies of geoinformatics and web technologies.',
-    aboutSecondParagraph: 'During my studies, I am building a strong foundation in spatial data analysis, GIS and map making. However, because connecting these disciplines with IT is my biggest passion, I spend my free time studying front-end development (HTML, CSS and JavaScript). My next goals are to learn the React framework and dive into back-end development using Python and database systems (SQL/MySQL).',
+    aboutSecondParagraph: 'During my studies, I am building a strong foundation in <strong class="skill">spatial data analysis, GIS and map making<strong>. However, because connecting these disciplines with IT is my biggest passion, I spend my free time studying front-end development (HTML, CSS and JavaScript). My next goals are to learn the React framework and dive into back-end development using Python and database systems (SQL).',
     aboutThirdParagraph: 'Currently, I am searching for an internship or a part-time job where I can put my knowledge into practice. I look forward to helping with real projects and gaining new experience.',
-    personalMotto: '"Anything is within reach, as long as your drive is strong enough."',
+    personalMotto: 'Personal motto: "Anything is within reach, as long as your drive is strong enough."',
     downloadCV: 'Download CV',
 
     // experience section
     crossCountryTitle: 'International level Cross-Country Skier',
-    crossCountryParagraph: 'Balancing a demanding daily training schedule with academic studies taught me how to manage my time effectively. Years of long-term athletic preparation built my goal-oriented mindset.',
+    crossCountryParagraph: 'Years of long-term athletic preparation built my <strong class="skill">goal-oriented mindset</strong>. Strong <strong class="skill">time management</strong> skills allowed me to balance a demanding daily training schedule with academic studies.',
     questionaryTitle: 'Field Data Collector',
     questionaryTime: 'October 2025',
-    questionaryParagraph: 'Sustainable Urban Mobility Plan City of Olomouc',
+    questionaryParagraph: 'Direct public communication and data collection for the City of Olomouc traffic survey, as part of the Sustainable Urban Mobility Plan (PUMMO). The main responsibility was to approach citizens in selected city districts and conduct structured questionnaire surveys regarding traffic conditions and parking capacities. Successfully connecting with people on the spot demanded high <strong class="skill">communicativeness and proactivity</strong>.',
 
     // projects section
     semestralMapTitle: 'Crisis and health infrastructure in Norway and Sweden',
     semestralMapTime: 'May 2026',
-    semestralMapParagraph: 'Cartographic project developed within the Digital Cartography course, resulting in a large-format (A2) desk map designed for strategic decision-making by public sector professionals.',
+    semestralMapParagraph: '<strong class="skill">Cartographic project</strong> developed within the Digital Cartography course, resulting in a large-format (A2) desk map designed for strategic decision-making by public sector professionals.',
     travelCalculatorTitle: 'Travel cost calculator',
     travelCalculatorTime: 'July 2026',
-    travelCalculatorParagraph: 'A JavaScript web application built to calculate and share travel expenses. It supports multiple languages and enables unit conversion, saving trip history, and generating QR codes for easy cost splitting. Focused on Vanilla JavaScript, this project helped me gain an understanding of DOM manipulation, event handling, and modules. To ensure clean code organization, I implemented the MVC (Model-View-Controller) architecture.',
+    travelCalculatorParagraph: 'A web application built to calculate and share travel expenses. It supports multiple languages and enables unit conversion, saving trip history, and generating QR codes for easy cost splitting. This project, focused on <strong class="skill">JavaScript programming</strong>, helped me gain an understanding of DOM manipulation, event handling, and modules. To ensure clean code organization, I implemented the MVC (Model-View-Controller) architecture.',
+    personalWebTitle: 'Personal web',
+    personalWebTime: 'August 2026',
+    personalWebParagraph: 'My personal interactive portfolio developed in HTML, CSS, and JavaScript. It features a minimalist responsive design, optimized for both mobile and desktop devices using a mobile-first approach. The core functionality is multi-language support.',
 
     // buttons
     liveDemoButton: 'Live demo',
     githubRepoButton: 'Github repo',
-    personalWebTitle: 'Personal web',
-    personalWebTime: 'August 2026',
-    personalWebParagraph: 'Portfolio in HTML, CSS and JavaScript',
     websiteButton: 'Website',
     departmentButton: 'Department',
     curriculumButton: 'Curriculum',
@@ -63,36 +63,36 @@ export const translations = {
     heroSubtitle: `Se zaměřením na <br><em class="highlight">front-end webové technologie</em>`,
 
     // about section
-    aboutFirstParagraph: 'Jsem studentem geoinformatiky na Univerzitě Palackého v Olomouci v České republice. Roky jsem se s velkým zápalem věnoval běhu na lyžích, ale s nástupem na vysokou školu mě studium pohltilo naplno. Disciplínu a cílevědomost, kterou jsem si za roky vrcholového sportu vybudoval, nyní přenáším do studia geoinformatiky a webových technologií.',
+    aboutFirstParagraph: 'Jsem studentem geoinformatiky na Univerzitě Palackého v Olomouci v České republice. Roky jsem se s velkým zápalem věnoval běhu na lyžích, ale s nástupem na vysokou školu mě studium pohltilo naplno. Disciplínu a cílevědomost, kterou jsem si po dobu vrcholového sportování vybudoval, nyní přenáším do studia geoinformatiky a webových technologií.',
 
-    aboutSecondParagraph: 'Během studia si buduji pevné základy v analýze prostorových dat, GIS a tvorbě map. Protože mě ale nejvíce naplňuje propojování těchto oborů s IT, věnuji svůj volný čas samostudiu front-endového vývoje (HTML, CSS a JavaScript). Mým dalším cílem je rozšířit své dovednosti o framework React a proniknout i do back-endového vývoje pomocí jazyka Python a práce s databázemi (SQL/MySQL).',
+    aboutSecondParagraph: 'Během studia si buduji pevné základy v <strong class="skill">analýze prostorových dat, GIS a tvorbě map</strong>. Protože mě ale nejvíce přitahuje propojování těchto oborů s IT, věnuji svůj volný čas samostudiu front-end vývoje (HTML, CSS a JavaScript). Mým dalším cílem je rozšířit své dovednosti o framework React a proniknout i do back-end vývoje prostřednictvím jazyka Python a práce s databázemi (SQL).',
 
     aboutThirdParagraph: 'Aktuálně hledám příležitosti pro studentské stáže nebo zkrácené úvazky, kde bych mohl své dosavadní znalosti uvést do praxe. Těším se na možnost pomáhat s reálnými projekty a nabrat nové zkušenosti.',
 
-    personalMotto: '"Vše je možné, pokud je vaše ‚chci‘ dostatečně silné."',
+    personalMotto: 'Osobní motto: "Vše je možné, pokud je vaše ‚chci‘ dostatečně silné."',
     downloadCV: 'Stáhnout životopis',
 
     // experience section
     crossCountryTitle: 'Běžec na lyžích na mezinárodní úrovni',
-    crossCountryParagraph: 'Skloubení náročného každodenního tréninku s vysokoškolským studiem mě naučilo efektivně si organizovat čas. Roky dlouhodobé sportovní přípravy ve mně vybudovaly cílevědomý přístup.',
-    questionaryTitle: 'Pracovník pro sběr dat v terénu',
+    crossCountryParagraph: 'Roky náročné sportovní přípravy ve mně vybudovaly <strong class="skill">cílevědomý přístup</strong>. Schopnost <strong class="skill">efektivní organizace času</strong> mi umožnila kombinovat každodenní trénink s vysokoškolským studiem.',
+    questionaryTitle: 'Sběrač dat v terénu',
     questionaryTime: 'říjen 2025',
-    questionaryParagraph: 'Dopravní průzkum v rámci Plánu udržitelné městské mobility Olomouc.',
+    questionaryParagraph: 'Přímá komunikace s veřejností a sběr dat pro dopravní průzkum Statutárního města Olomouce v rámci Plánu udržitelné městské mobility Olomouc (PUMMO). Hlavní náplní práce bylo oslovovat občany vybraných městských čtvrtí a provádět s nimi strukturovaná dotazníková šetření ohledně stavu dopravy a parkovacích kapacit. Pro rychlé navázání kontaktu s neznámými lidmi byla nutná <strong class="skill">komunikativnost a proaktivita</strong>.',
 
     // projects section
     semestralMapTitle: 'Krizová a zdravotnická infrastruktura v Norsku a Švédsku',
     semestralMapTime: 'květen 2026',
-    semestralMapParagraph: 'Kartografický projekt vytvořený v rámci předmětu Digitální kartografie. Jeho výsledkem je velkoformátová (A2) stolní mapa navržená pro strategické rozhodování pracovníků veřejného sektoru.',
+    semestralMapParagraph: '<strong class="skill">Kartografický projekt</strong> vytvořený v rámci předmětu Digitální kartografie. Jeho výsledkem je velkoformátová (A2) stolní mapa navržená pro strategické rozhodování pracovníků veřejného sektoru.',
     travelCalculatorTitle: 'Kalkulačka cestovních nákladů',
     travelCalculatorTime: 'červenec 2026',
-    travelCalculatorParagraph: 'JavaScriptová webová aplikace vytvořená k výpočtu a sdílení cestovních nákladů. Podporuje více jazyků a umožňuje převody jednotek, ukládání historie cest a generování QR kódů pro snadné rozdělení výdajů. Tento projekt, zaměřený na čistý (Vanilla) JavaScript, mi pomohl osvojit si manipulaci s DOM, práci s událostmi (event handling) a využití modulů. Pro zajištění přehledné struktury kódu jsem implementoval architekturu MVC (Model-View-Controller).',
+    travelCalculatorParagraph: 'Webová aplikace vytvořená k výpočtu a sdílení cestovních nákladů. Podporuje více jazyků a umožňuje převody jednotek, ukládání historie cest a generování QR kódů pro snadné rozdělení výdajů. Tento projekt, zaměřený na <strong class="skill">programování v JavaScriptu</strong>, mi pomohl osvojit si manipulaci s DOM, práci s událostmi (event handling) a využití modulů. Pro zajištění přehledné struktury kódu jsem implementoval architekturu MVC (Model-View-Controller).',
+    personalWebTitle: 'Osobní web',
+    personalWebTime: 'srpen 2026',
+    personalWebParagraph: 'Mé osobní interaktivní portfolio v <strong class="skill">HTML, CSS a JavaScriptu</strong>. Vyznačuje se minimalistickým <strong class="skill">responsivním designem</strong>, optimalizovaným pro mobilní i desktopová zařízení díky přístupu mobile-first. Hlavní funkcionalitou je podpora více jazyků. (později darkmode atd. doplnit)',
 
     // buttons
     liveDemoButton: 'Spustit aplikaci',
     githubRepoButton: 'Zdrojový kód',
-    personalWebTitle: 'Osobní web',
-    personalWebTime: 'srpen 2026',
-    personalWebParagraph: 'Portfolio v HTML, CSS a JavaScriptu',
     websiteButton: 'Web',
     departmentButton: 'Katedra',
     curriculumButton: 'Studijní plán',
