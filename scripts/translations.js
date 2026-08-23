d9f150c7aefe7eccc76ef282af0e64eef2ac748c
breznaM <breznamatej22@gmail.com>
2026-08-23 21:15:46 +0200

    Add <abbr> inline elements

--- scripts/translations.js
+++ scripts/translations.js
@@ -14,8 +14,9 @@ export const translations = {
     heroSubtitle: `Self-taught in <br><em class="highlight">front-end web technologies</em>`,
 
     // about section
+    aboutTitle: 'About me',
     aboutFirstParagraph: 'I am a student of geoinformatics at Palacký University Olomouc, Czechia. For years, I was a competitive cross-country skier with great enthusiasm, but as soon as I started university, my studies took my full attention. The discipline and determination I built during my years in elite sports are qualities I now apply to my studies of geoinformatics and web technologies.',
-    aboutSecondParagraph: 'During my studies, I am building a strong foundation in <strong class="skill">spatial data analysis, GIS and map making</strong>. However, because connecting these disciplines with IT is my biggest passion, I spend my free time studying front-end development (HTML, CSS and JavaScript). My next goals are to learn the React framework and dive into back-end development using Python and database systems (SQL).',
+    aboutSecondParagraph: 'During my studies, I am building a strong foundation in <strong class="skill">spatial data analysis, <abbr title="Geographic Information System">GIS</abbr> and map making</strong>. However, because connecting these disciplines with <abbr title="Information Technology">IT</abbr> is my biggest passion, I spend my free time studying front-end development (<abbr title="HyperText Markup Language">HTML</abbr>, <abbr title="Cascading Style Sheets">CSS</abbr> and JavaScript). My next goals are to learn the React framework and dive into back-end development using Python and database systems (<abbr title="Structured Query Language">SQL</abbr>).',
     aboutThirdParagraph: 'Currently, I am searching for an internship or a part-time job where I can put my knowledge into practice. I look forward to helping with real projects and gaining new experience.',
     personalMotto: 'Personal motto: "Anything is within reach, as long as your drive is strong enough."',
     downloadCV: 'Download CV',
@@ -25,7 +26,7 @@ export const translations = {
     crossCountryParagraph: 'Years of long-term athletic preparation built my <strong class="skill">goal-oriented mindset</strong>. Strong <strong class="skill">time management</strong> skills allowed me to balance a demanding daily training schedule with academic studies.',
     questionaryTitle: 'Field Data Collector',
     questionaryTime: 'October 2025',
-    questionaryParagraph: 'Direct public communication and data collection for the City of Olomouc traffic survey, as part of the Sustainable Urban Mobility Plan (PUMMO). The main responsibility was to approach citizens in selected city districts and conduct structured questionnaire surveys regarding traffic conditions and parking capacities. Successfully connecting with people on the spot demanded high <strong class="skill">communicativeness and proactivity</strong>.',
+    questionaryParagraph: 'Direct public communication and data collection for the City of Olomouc traffic survey, as part of the Sustainable Urban Mobility Plan (<abbr title="Plán udržitelné městské mobility Olomouc">PUMMO</abbr>). The main responsibility was to approach citizens in selected city districts and conduct structured questionnaire surveys regarding traffic conditions and parking capacities. Successfully connecting with people on the spot demanded high <strong class="skill">communicativeness and proactivity</strong>.',
 
     // projects section
     semestralMapTitle: 'Crisis and health infrastructure in Norway and Sweden',
@@ -33,10 +34,10 @@ export const translations = {
     semestralMapParagraph: '<strong class="skill">Cartographic project</strong> developed within the Digital Cartography course, resulting in a large-format (A2) desk map designed for strategic decision-making by public sector professionals.',
     travelCalculatorTitle: 'Travel cost calculator',
     travelCalculatorTime: 'July 2026',
-    travelCalculatorParagraph: 'A web application built to calculate and share travel expenses. It supports multiple languages and enables unit conversion, saving trip history, and generating QR codes for easy cost splitting. This project, focused on <strong class="skill">JavaScript programming</strong>, helped me gain an understanding of DOM manipulation, event handling, and modules. To ensure clean code organization, I implemented the MVC (Model-View-Controller) architecture.',
+    travelCalculatorParagraph: 'A web application built to calculate and share travel expenses. It supports multiple languages and enables unit conversion, saving trip history, and generating QR codes for easy cost splitting. This project, focused on <strong class="skill">JavaScript programming</strong>, helped me gain an understanding of <abbr title="Document Object Model">DOM</abbr> manipulation, event handling, and modules. To ensure clean code organization, I implemented the <abbr title="Model-View-Controller">MVC</abbr> architecture.',
     personalWebTitle: 'Personal web',
     personalWebTime: 'August 2026',
-    personalWebParagraph: 'My personal interactive portfolio developed in <strong class="skill">HTML, CSS, and JavaScript</strong>. It features a minimalist <strong class="skill">responsive design</strong>, optimized for both mobile and desktop devices using a mobile-first approach. The core functionality is multi-language support.',
+    personalWebParagraph: 'My personal interactive portfolio developed in <strong class="skill"><abbr title="HyperText Markup Language">HTML</abbr>, <abbr title="Cascading Style Sheets">CSS</abbr>, and JavaScript</strong>. It features a minimalist <strong class="skill">responsive design</strong>, optimized for both mobile and desktop devices using a mobile-first approach. The core functionality is multi-language support.',
 
     // buttons
     liveDemoButton: 'Live demo',
@@ -69,6 +70,7 @@ export const translations = {
     heroSubtitle: `Se zaměřením na <br><em class="highlight">front-end webové technologie</em>`,
 
     // about section
+    aboutTitle: 'O mně',
     aboutFirstParagraph: 'Jsem studentem geoinformatiky na Univerzitě Palackého v Olomouci v České republice. Roky jsem se s velkým zápalem věnoval běhu na lyžích, ale s nástupem na vysokou školu mě studium pohltilo naplno. Disciplínu a cílevědomost, kterou jsem si po dobu vrcholového sportování vybudoval, nyní přenáším do studia geoinformatiky a webových technologií.',
 
     aboutSecondParagraph: 'Během studia si buduji pevné základy v <strong class="skill">analýze prostorových dat, GIS a tvorbě map</strong>. Protože mě ale nejvíce přitahuje propojování těchto oborů s IT, věnuji svůj volný čas samostudiu front-end vývoje (HTML, CSS a JavaScript). Mým dalším cílem je rozšířit své dovednosti o framework React a proniknout i do back-end vývoje prostřednictvím jazyka Python a práce s databázemi (SQL).',
