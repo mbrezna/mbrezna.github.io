b24d9b069055a72d06b6b51f6f5a3b981891a12b
breznaM <breznamatej22@gmail.com>
2026-08-25 20:47:41 +0200

    Improve Personal Web article

--- scripts/translations.js
+++ scripts/translations.js
@@ -37,7 +37,12 @@ export const translations = {
     travelCalculatorParagraph: 'A web application built to calculate and share travel expenses. It supports multiple languages and enables unit conversion, saving trip history, and generating QR codes for easy cost splitting. This project, focused on <strong class="skill">JavaScript programming</strong>, helped me gain an understanding of <abbr title="Document Object Model">DOM</abbr> manipulation, event handling, and modules. To ensure clean code organization, I implemented the <abbr title="Model-View-Controller">MVC</abbr> architecture.',
     personalWebTitle: 'Personal web',
     personalWebTime: 'August 2026',
-    personalWebParagraph: 'My personal interactive portfolio developed in <strong class="skill"><abbr title="HyperText Markup Language">HTML</abbr>, <abbr title="Cascading Style Sheets">CSS</abbr>, and JavaScript</strong>. It features a minimalist <strong class="skill">responsive design</strong>, optimized for both mobile and desktop devices using a mobile-first approach. The core functionality is multi-language support.',
+    personalWebParagraph: 'My personal interactive portfolio developed in <strong class="skill"><abbr title="HyperText Markup Language">HTML</abbr>, <abbr title="Cascading Style Sheets">CSS</abbr>, and JavaScript</strong>. It features a minimalist <strong class="skill">responsive design</strong>, optimized for both mobile and desktop devices using a mobile-first approach. Key features include <strong class="skill">multi-language support</strong> and <strong class="skill">theme toggling</strong> between light and dark modes. Instant page loading and seamless indexing are ensured by <strong class="skill">100% valid and semantically clean code</strong> alongside full accessibility for assistive technologies.',
+    pageAudit: 'Performance & Accessibility audit according to Google PageSpeed Insights',
+    pageAuditPerformance: 'Performance',
+    pageAuditAccesibility: 'Accesibility',
+    pageAuditBestPractises: 'Best practises',
+    pageAuditSEO: '<abbr title="Search Engine Optimization">SEO</abbr>',
 
     // buttons
     liveDemoButton: 'Live demo',
@@ -80,7 +85,7 @@ export const translations = {
     aboutTitle: 'O mně',
     aboutFirstParagraph: 'Jsem studentem geoinformatiky na Univerzitě Palackého v Olomouci v České republice. Roky jsem se s velkým zápalem věnoval běhu na lyžích, ale s nástupem na vysokou školu mě studium pohltilo naplno. Disciplínu a cílevědomost, kterou jsem si po dobu vrcholového sportování vybudoval, nyní přenáším do studia geoinformatiky a webových technologií.',
 
-    aboutSecondParagraph: 'Během studia si buduji pevné základy v <strong class="skill">analýze prostorových dat, GIS a tvorbě map</strong>. Protože mě ale nejvíce přitahuje propojování těchto oborů s IT, věnuji svůj volný čas samostudiu front-end vývoje (HTML, CSS a JavaScript). Mým dalším cílem je rozšířit své dovednosti o framework React a proniknout i do back-end vývoje prostřednictvím jazyka Python a práce s databázemi (SQL).',
+    aboutSecondParagraph: 'Během studia si buduji pevné základy v <strong class="skill">analýze prostorových dat, <abbr title="Geografický informační systém">GIS</abbr> a tvorbě map</strong>. Protože mě ale nejvíce přitahuje propojování těchto oborů s <abbr title="informační technologie">IT</abbr>, věnuji svůj volný čas samostudiu front-end vývoje (<abbr title="Hypertextový značkovací jazyk">HTML</abbr>, <abbr title="Kaskádové styly">CSS</abbr> a JavaScript). Mým dalším cílem je rozšířit své dovednosti o framework React a proniknout i do back-end vývoje prostřednictvím jazyka Python a práce s databázemi (<abbr title="Strukturovaný dotazovací jazyk">SQL</abbr>).',
 
     aboutThirdParagraph: 'Aktuálně hledám příležitosti pro studentské stáže nebo zkrácené úvazky, kde bych mohl své dosavadní znalosti uvést do praxe. Těším se na možnost pomáhat s reálnými projekty a nabrat nové zkušenosti.',
 
@@ -92,7 +97,7 @@ export const translations = {
     crossCountryParagraph: 'Roky náročné sportovní přípravy ve mně vybudovaly <strong class="skill">cílevědomý přístup</strong>. Schopnost <strong class="skill">efektivní organizace času</strong> mi umožnila kombinovat každodenní trénink s vysokoškolským studiem.',
     questionaryTitle: 'Sběrač dat v terénu',
     questionaryTime: 'říjen 2025',
-    questionaryParagraph: 'Přímá komunikace s veřejností a sběr dat pro dopravní průzkum Statutárního města Olomouce v rámci Plánu udržitelné městské mobility Olomouc (PUMMO). Hlavní náplní práce bylo oslovovat občany vybraných městských čtvrtí a provádět s nimi strukturovaná dotazníková šetření ohledně stavu dopravy a parkovacích kapacit. Pro rychlé navázání kontaktu s neznámými lidmi byla nutná <strong class="skill">komunikativnost a proaktivita</strong>.',
+    questionaryParagraph: 'Přímá komunikace s veřejností a sběr dat pro dopravní průzkum Statutárního města Olomouce v rámci Plánu udržitelné městské mobility Olomouc (<abbr title="Plán udržitelné městské mobility Olomouc">PUMMO</abbr>). Hlavní náplní práce bylo oslovovat občany vybraných městských čtvrtí a provádět s nimi strukturovaná dotazníková šetření ohledně stavu dopravy a parkovacích kapacit. Pro rychlé navázání kontaktu s neznámými lidmi byla nutná <strong class="skill">komunikativnost a proaktivita</strong>.',
 
     // projects section
     semestralMapTitle: 'Krizová a zdravotnická infrastruktura v Norsku a Švédsku',
@@ -100,10 +105,15 @@ export const translations = {
     semestralMapParagraph: '<strong class="skill">Kartografický projekt</strong> vytvořený v rámci předmětu Digitální kartografie. Jeho výsledkem je velkoformátová (A2) stolní mapa navržená pro strategické rozhodování pracovníků veřejného sektoru.',
     travelCalculatorTitle: 'Kalkulačka cestovních nákladů',
     travelCalculatorTime: 'červenec 2026',
-    travelCalculatorParagraph: 'Webová aplikace vytvořená k výpočtu a sdílení cestovních nákladů. Podporuje více jazyků a umožňuje převody jednotek, ukládání historie cest a generování QR kódů pro snadné rozdělení výdajů. Tento projekt, zaměřený na <strong class="skill">programování v JavaScriptu</strong>, mi pomohl osvojit si manipulaci s DOM, práci s událostmi (event handling) a využití modulů. Pro zajištění přehledné struktury kódu jsem implementoval architekturu MVC (Model-View-Controller).',
+    travelCalculatorParagraph: 'Webová aplikace vytvořená k výpočtu a sdílení cestovních nákladů. Podporuje více jazyků a umožňuje převody jednotek, ukládání historie cest a generování QR kódů pro snadné rozdělení výdajů. Tento projekt, zaměřený na <strong class="skill">programování v JavaScriptu</strong>, mi pomohl osvojit si manipulaci s <abbr title="Objektový model dokumentu">DOM</abbr>, práci s událostmi (event handling) a využití modulů. Pro zajištění přehledné struktury kódu jsem implementoval architekturu <abbr title="Model-View-Controller">MVC</abbr>.',
     personalWebTitle: 'Osobní web',
     personalWebTime: 'srpen 2026',
-    personalWebParagraph: 'Mé osobní interaktivní portfolio v <strong class="skill">HTML, CSS a JavaScriptu</strong>. Vyznačuje se minimalistickým <strong class="skill">responsivním designem</strong>, optimalizovaným pro mobilní i desktopová zařízení díky přístupu mobile-first. Hlavní funkcionalitou je podpora více jazyků. (později darkmode atd. doplnit)',
+    personalWebParagraph: 'Mé osobní interaktivní portfolio v <strong class="skill"><abbr title="Hypertextový značkovací jazyk">HTML</abbr>, <abbr title="Kaskádové styly">CSS</abbr> a JavaScriptu</strong>. Vyznačuje se minimalistickým <strong class="skill">responsivním designem</strong>, optimalizovaným pro mobilní i desktopová zařízení díky přístupu mobile-first. Hlavní funkcionalitou je <strong class="skill">podpora více jazyků</strong> a <strong class="skill">přepínání zobrazovacího módu</strong> mezi světlým a tmavým režimem. Okamžité načítání stránky a dobrou indexaci zajišťuje <strong class="skill">100% validní a sémanticky čistý kód</strong> spolu se zajištěnou přístupností pro asistenční technologie.',
+    pageAudit: 'Hodnocení výkonu a přístupnosti podle Google PageSpeed Insights',
+    pageAuditPerformance: 'Výkon',
+    pageAuditAccesibility: 'Přístupnost',
+    pageAuditBestPractises: 'Doporučené postupy',
+    pageAuditSEO: '<abbr title="Optimalizace pro vyhledávače">SEO</abbr>',
 
     // buttons
     liveDemoButton: 'Spustit aplikaci',
